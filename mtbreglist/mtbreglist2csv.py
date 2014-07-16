2a46381dfb450e655a4395947b4a22bc80bf3922
Sefa Kilic <sefakilic@gmail.com>
2014-07-16 16:28:36 -0400

    added TF accession column

--- mtbreglist/mtbreglist2csv.py
+++ mtbreglist/mtbreglist2csv.py
@@ -17,7 +17,7 @@ def get_genome(acc=MTB_GENOME_ACCESSION):
     # if not exist, download from NCBI genome database
     if not os.path.isfile(genbank_file):
         net_handle = Entrez.efetch(db='nuccore', id=acc,
-                                   retmode='gbwithparts',rettype='text')
+                                   retmode='gbwithparts', rettype='text')
         out_handle = open(genbank_file, 'w')
         out_handle.write(net_handle.read())
         net_handle.close()
@@ -29,14 +29,21 @@ def get_genome(acc=MTB_GENOME_ACCESSION):
 def get_gene(genome_record, locus_tag):
     """Find the sequence feature for a particular gene"""
     genes = [f for f in genome_record.features
-             if f.type=='gene' and f.qualifiers['locus_tag'][0]==locus_tag]
+             if f.type == 'gene' and f.qualifiers['locus_tag'][0] == locus_tag]
     assert len(genes) == 1, "Multiple seqfeature for locus_tag %s" % locus_tag
     return head(genes)
 
+def get_prot(genome_record, locus_tag):
+    """Find protein feature in Genbank file for a given locus tag"""
+    prots = [f for f in genome_record.features
+             if f.type == 'CDS' and f.qualifiers['locus_tag'][0] == locus_tag]
+    assert len(prots) == 1
+    return head(prots)
+
 def repair_mtb_locus_tags(mtb_export_file):
-    """Mycobacterium tuberculosis has locus tags in format (Rv\d+). However,
-    for some reason, some of the locus tags in MtbRegList export don't match
-    with any gene in the NCBI genome record. It seems that some locus tags (RNA
+    """Mycobacterium tuberculosis has locus tags in format (Rv\d+). However, for
+    some reason, some of the locus tags in MtbRegList export don't match with
+    any gene in the NCBI genome record. It seems that some locus tags (RNA
     coding genes) have been updated at some point in NCBI RefSeq database. These
     locus tags in MtbRegList have been replaced with the one in the genbank file
     if start and end positions match.
@@ -55,7 +62,7 @@ def repair_mtb_locus_tags(mtb_export_file):
     with open(mtb_export_file) as f:
         all_text = f.read()
     # Replace old locus tags with new ones
-    for (old,new) in replacements:
+    for (old, new) in replacements:
         all_text = all_text.replace(old, new)
     with open(mtb_export_file, 'w') as f:
         f.write(all_text)
@@ -97,7 +104,7 @@ def binding_site_loc_one(genome_record, fg_locus_tag, dist5, dist3):
 def parse_signature(signature):
     """Parse the signature as defined in MtbRegList export and return actual
     sequence where spacer is denoted with sequence of N's.
-    
+
     The signature has the pattern S@m[/SP/S@m] where S stands for sequence box,
     SP for spacing and m for mismatch count relative to its root pattern. For
     more info, see MtbRegList paper.
@@ -138,6 +145,14 @@ def identify_binding_site(genome_record, df_row):
     print start, end, strand, site
     return pd.Series({'start': start, 'end': end, 'strand': strand, 'site':site})
 
+def get_protein_accession(genome_record, df_row):
+    """Given a row of the data frame, find the protein accession for the TF
+    using its locus tag"""
+    locus_tag = df_row["Recognized by"]
+    prot = get_prot(genome_record, locus_tag)
+    prot_acc = prot.qualifiers['protein_id'][0].split('.')[0]
+    return pd.Series({'TF accession': prot_acc})
+
 def read_mtbreglist(export_file="results.txt"):
     """Given the tab delimited text file exported from MtbRegList database, read
     the data that contains promoters, transcription factor binding sites and
@@ -153,15 +168,25 @@ def read_mtbreglist(export_file="results.txt"):
     df['ambiguous_seq'] = df.apply(lambda x:parse_signature(x['Signature']),axis=1)
     # Get genome
     genome_rec = get_genome(MTB_GENOME_ACCESSION)
+
+    df.apply(lambda x: get_protein_accession(genome_rec, x), axis=1)
+    
     # identify binding sites
     df = pd.merge(df,
                   df.apply(lambda x: identify_binding_site(genome_rec, x), axis=1),
                   left_index=True,
                   right_index=True)
+
+    df = pd.merge(df,
+                  df.apply(lambda x: get_protein_accession(genome_rec, x), axis=1),
+                  left_index=True,
+                  right_index=True)
+    
     # rename some columns
     df.rename(columns={'Recognized by': 'TF locus_tag'}, inplace=True)
     df['Genome'] = MTB_GENOME_ACCESSION
     cols_to_write = ['Genome',
+                     'TF accession',
                      'TF locus_tag',
                      'Synonyms',
                      'start',
