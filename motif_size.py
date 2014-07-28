4cf08ad461e89eea5d8f793f5e83cc88df6d91a8
Sefa Kilic <sefakilic@gmail.com>
2014-07-28 09:36:40 -0400

    csvs merged

--- motif_size.py
+++ motif_size.py
@@ -1,6 +1,28 @@
 """Motif size for each TF/genome"""
 
 import pandas as pd
+import os
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email = 'sefa1@umbc.edu'
+
+def get_org_name(acc):
+    """Get genome record"""
+    downloads_dir = '/home/sefa/Downloads'
+    genbank_file = os.path.join(downloads_dir, acc+'.gb')
+    # if not exist, download from NCBI genome database
+    if not os.path.isfile(genbank_file):
+        print "Downloading genbank file", acc
+        net_handle = Entrez.efetch(db='nuccore', id=acc,
+                                   retmode='gbwithparts', rettype='text')
+        out_handle = open(genbank_file, 'w')
+        out_handle.write(net_handle.read())
+        net_handle.close()
+        out_handle.close()
+    # read record
+    record = SeqIO.read(genbank_file, 'genbank')
+    return record
+
 
 def group_sites(df):
     """Group binding sites by TF accession and genome accession"""
@@ -19,5 +41,7 @@ def main():
     df = pd.read_csv('tfbs_data_merged.tsv', sep='\t')
     groups = group_sites(df)
     motif_sizes = groups.size().reset_index().rename(columns={0:'motif_size'})
+    motif_sizes['organism'] = motif_sizes.apply(
+        lambda row: get_org_name(row['genome_accession']))
     motif_sizes.to_csv('motif_sizes.csv', index=False)
     
